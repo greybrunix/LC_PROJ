ccd3437053d088687d58762f7ddfc49dc424ee3c
Bruno Gião <a96544@alunos.uminho.pt>
2023-11-24 22:21:08 +0000

    aslkdj

--- TP3_extra/wpc.hs
+++ TP3_extra/wpc.hs
@@ -1,34 +1,19 @@
 module WPC_Parser where
 
 import Parsing
-
 {-
-WPC GRAMMAR
-<flux>        ::= SKIP RPAREN
-                | LPAREN <comp> 
-                | LPAREN <choice>
-<comp>        ::= <command> COMP <flux>
-<choice>      ::= <flux> CHOICE <flux> 
-<command>     ::= <assume> 
-                | <force> 
-                | <attrib> 
-                | <havoc>
-<assume>      ::= ASSUME <log_expr>
-<force>       ::= FORCE <log_expr>
-<attrib>      ::= <vector> ATTRIB <expr_vector>
-<havoc>       ::= HAVOC <vector>
 <vector>      ::= LPAREN <vector_ids>
-		| ID
-<vector_ids>  ::= ID <vec_ids_m> 
+                | ID
+<vector_ids>  ::= ID <vec_ids_m>
                 | RPAREN
-<vec_ids_m>   ::= COMMA <vector_ids> 
+<vec_ids_m>   ::= COMMA <vector_ids>
                 | <vector_ids>
 <log_expr>    ::= <log_term> op_ex <log_expr>
                 | <log_term>
 <log_term>    ::= <log_factor> op_term <log_term>
                 | <log_factor>
 <log_factor>  ::= LPAREN <log_factor> RPAREN
-                | ID 
+                | ID
                 | INT
                 | True
                 | False
@@ -38,54 +23,73 @@ WPC GRAMMAR
 <op_ex>       ::= LT | LE | GT | GE | EQ | NE
 <expr_vector> ::= LPAREN <exprs>
                 | <expr>
-<exprs>       ::= <expr> <exprs_m> 
+<exprs>       ::= <expr> <exprs_m>
                 | RPAREN
-<exprs_m>     ::= COMMA  <exprs> 
+<exprs_m>     ::= COMMA  <exprs>
                 | <exprs>
 <expr>        ::=  <term> ADD <expr> | <term> SUB <expr> | <term>
 <term>        ::=  <factor> MUL <term> | <factor> DIV <term> | <factor>
 <factor>      ::=  LPAREN <expr> RPAREN | INT | ID
 -}
 
--- Parser (BTree (Either String Char))
+-- Parser (BTree String))
 
-flux :: Parser (BTree (Either String Char))
-flux = do string "skip" 
-          char ')'
-          return (Node (Right '$') Empty Empty)
+flux :: Parser (BTree String)
+flux = do string "skip"
+          return (Node "$" Empty Empty)
    <|> comp
    <|> do char '('
           x <- choice
-          return x
+          char ')'
+          return (x) 
    <|> do char '('
           x <- flux
+          char ')'
           return (x)
 comp = do x <- command
           char ';'
           y <- flux
-          return (Node (Right ';') x y)
+          return (Node ";" x y)
 choice = do x <- flux
             char '|'
             char '|'
             y <- flux
-            return (Node (Right '|') x y)
+            return (Node "|" x y)
 command = assume
       <|> force
       <|> havoc
-      <|> attrib
+--      <|> attrib
 assume = do string "assume "
             x <- log_expr
-            return (Node (Right 'A') x Empty)
+            return (Node "A" x Empty)
 force  = do string "force " <|> string "assert "
             x <- log_expr
-            return (Node (Right 'F') x Empty)
+            return (Node "F" x Empty)
 havoc  = do string "havoc "
             x <- vector
-            return (Node (Right 'H') x Empty)
+            return (Node "H" x Empty)
+
+{-
 attrib = do x <- vector
             char '<'
             char '-'
             y <- expr_vector
-            return (Node (Right 'a') x y)
-log_expr = do x <- ident ; return (Node (Left x) Empty Empty)
-       <|> do askld
+            return (Node ":=" x y)
+-}
+vector = do char '('
+            x <- vector_ids
+            return (Node "," x Empty)
+        <|> do x <- ident
+               return (Node x Empty Empty)
+vector_ids = do x <- ident
+                y <- vector_ids_m
+                return (Node "," (Node x Empty Empty) y)
+            <|> do x <- ident ; char ')'
+                   return (Node x Empty Empty)
+vector_ids_m = do char ','
+                  x <- vector_ids
+                  return x
+
+
+log_expr = do x <- ident ; return (Node x Empty Empty)
+--       <|> do askld
